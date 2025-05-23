30c19212fc4a89909c5f92b286cf5e471ddde185
Ayush Lohani <lohaniayush97@gmail.com>
2025-05-23 21:41:19 +0530

    updated scss

--- Schedular/src/components/Slot/Slot.jsx
+++ Schedular/src/components/Slot/Slot.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Slot.scss";
 
 const Slot = ({ onSlotClick }) => {
   const slots = Array.from({ length: 11 }, (_, i) => `${8 + i}:00`);
 
   return (
-    <div>
+    <div className="slot">
       {slots.map((i) => (
         <button key={i} onClick={onSlotClick}>
           {i}
