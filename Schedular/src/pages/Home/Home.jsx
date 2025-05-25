38d2edc988febd28fa2a9a024f73acc7dcf73204
abhishek lohani <125081866+ayushlohani@users.noreply.github.com>
2025-05-26 00:01:37 +0530

    sideposter added

--- Schedular/src/pages/Home/Home.jsx
+++ Schedular/src/pages/Home/Home.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Cards from "../../components/Cards/cards";
 import doctorList from "../../data/DoctorData";
 import "./Home.scss"
-import Navbar from "../../components/Navbar/Navbar";
+import Calendar from "react-calendar"
 
 const Home = () => {
   return (
       <div className="home-scroll-container">
+      <div className="left">
       {doctorList.map((doctor, index) => (
         <Cards key={index} doctor={doctor} />
       ))}
+      </div>
+      <div className="right">
+        <img src="https://static.vecteezy.com/system/resources/previews/013/141/034/non_2x/book-doctor-appointment-card-template-schedule-hospital-visit-editable-social-media-post-design-flat-color-illustration-for-poster-web-banner-ecard-vector.jpg" />
+      </div>
     </div>
     
   );
