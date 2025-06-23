3e814c0b4e814b86960b3548e72f3e1e0a10fe7b
Ayush Lohani <lohaniayush97@gmail.com>
2025-06-23 18:54:19 +0530

    routing fixed

--- Schedular/src/pages/Category/Category.jsx
+++ Schedular/src/pages/Category/Category.jsx
@@ -13,13 +13,13 @@ const Category = () => {
     <div className="Category">
       <h1>What are you looking for?</h1>
       <div className="cat">
-        <div onClick={() => onNav("Mental")} className="cat-cont">
+        <div onClick={() => onNav("mental")} className="cat-cont">
           Mental
         </div>
-        <div onClick={() => onNav("Physical")} className="cat-cont">
+        <div onClick={() => onNav("physical")} className="cat-cont">
           Physical
         </div>
-        <div onClick={() => onNav("Financial")} className="cat-cont">
+        <div onClick={() => onNav("financial")} className="cat-cont">
           Financial
         </div>
       </div>
