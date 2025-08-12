3089d64addcf7d4c5c5a4046269efdedd512b486
Ayush Lohani <lohaniayush97@gmail.com>
2025-08-12 11:35:12 +0530

    time decreae api calling

--- Schedular/src/pages/AdvisorDashboard/AdvisorDashboard.jsx
+++ Schedular/src/pages/AdvisorDashboard/AdvisorDashboard.jsx
@@ -72,7 +72,7 @@ export default function AdvisorDashboard() {
       fetchQuickSessions();
       intervalId = setInterval(() => {
         fetchQuickSessions();
-      }, 5000);
+      }, 3000);
     }
 
     return () => {
