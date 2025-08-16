1cfb759df664decbe8bd6fc452d2cbb2d966a259
Ayush Lohani <lohaniayush97@gmail.com>
2025-08-16 12:38:43 +0530

    new Changes

--- Schedular/src/store/roleSlice.js
+++ Schedular/src/store/roleSlice.js
@@ -1,15 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const roleSlice = createSlice({
-    name:"User",
-    initialState:"",
-    reducers:{
-        loginRole:(store,action)=>{
-            return action.payload;
-        },
-        logoutRole:(store,action)=>{
-            return "";
-        }
+    name: "role",
+    initialState: "",
+    reducers: {
+        loginRole: (state, action) => action.payload,
+        logoutRole: () => ""
     }
 });
 
